46be9b3965f3fa9015f1fd413becd7de4395abc9
Matt Broussard <mattb@cs.utexas.edu>
2013-11-14 02:29:11 -0600

    added support for low-detail static map images provided by the department

--- js/config.js
+++ js/config.js
@@ -28,3 +28,62 @@ var config_research = [
 		"page" : "html/research/dummy.html"
 	}
 ];
+
+var config_maps = [
+	{
+		"floor" : "1N",
+		"image" : "img/maps/static/gdcfloorplan1N.png"
+	},
+	{
+		"floor" : "1S",
+		"image" : "img/maps/static/gdcfloorplan1S.png"
+	},
+	{
+		"floor" : "2N",
+		"image" : "img/maps/static/gdcfloorplan2N.png"
+	},
+	{
+		"floor" : "2S",
+		"image" : "img/maps/static/gdcfloorplan2S.png"
+	},
+	{
+		"floor" : "3N",
+		"image" : "img/maps/static/gdcfloorplan3N.png"
+	},
+	{
+		"floor" : "3S",
+		"image" : "img/maps/static/gdcfloorplan3S.png"
+	},
+	{
+		"floor" : "4N",
+		"image" : "img/maps/static/gdcfloorplan4N.png"
+	},
+	{
+		"floor" : "4S",
+		"image" : "img/maps/static/gdcfloorplan4S.png"
+	},
+	{
+		"floor" : "5N",
+		"image" : "img/maps/static/gdcfloorplan5N.png"
+	},
+	{
+		"floor" : "5S",
+		"image" : "img/maps/static/gdcfloorplan5S.png"
+	},
+	{
+		"floor" : "6N",
+		"image" : "img/maps/static/gdcfloorplan6N.png"
+	},
+	{
+		"floor" : "6S",
+		"image" : "img/maps/static/gdcfloorplan6S.png"
+	},
+	{
+		"floor" : "7N",
+		"image" : "img/maps/static/gdcfloorplan7N.png"
+	},
+	{
+		"floor" : "7S",
+		"image" : "img/maps/static/gdcfloorplan7S.png"
+	}
+];
---
+++ js/static_map.js
@@ -0,0 +1,76 @@
+
+var mapCurLoc = "3N";
+
+/* scrollSafeClick() implemented in menu.js */
+
+function mapPopup(room, $elem) {
+
+	var name = $elem.attr("data-subtitle");
+	var phone = $elem.attr("data-phone");
+	
+	var rec = directoryDataForRoom(room);
+	if (rec!=null) {
+		if (name!="") {}
+		else if (rec.lName) name = rec.fName+" "+rec.lName;
+		else if (rec.title) name = rec.title;
+		else if (rec.name) name = rec.name;
+		if (rec.phone && phone=="") phone = formatPhone(rec.phone);
+	}
+	
+	$("#map_popup_title").text("GDC " + room);
+	$("#map_popup_subtitle").text(name);
+	$("#map_popup_phone").text(phone);
+	
+	var x = $elem.offset().left + Math.round($elem.width()/2);
+	var y = $elem.offset().top + Math.round($elem.height()/2) - 50;
+	
+	var h = $("#map_popup").height() + 20;
+	
+	var nx = x - 60;
+	var ny = y - h;
+	
+	$("#map_popup").css({"left":nx+"px","top":ny+"px"}).show();
+
+}
+
+function mapClick() {
+
+	mapPopup($(this).text(), $(this));
+
+}
+
+$(function() {
+
+	enableMenuButton("map");
+	
+	//init the floor images
+	for (var i in config_maps) {
+		$("<img>")
+			.attr("src", config_maps[i]["image"])
+			.attr("id", "map_image_"+config_maps[i]["floor"])
+			.appendTo("#map_container > div")
+			.hide();
+	}
+
+	//when clicking the floor chooser, update the control
+	$("#map_floor_chooser button").click(function() {
+		$("#map_floor_chooser button").removeClass("on");
+		$(this).addClass("on");
+		mapCurLoc = $(this).attr("id").replace("map_floor_chooser_button_","") + mapCurLoc.substring(1);
+		$("#map_container > div > img").hide();
+		$("#map_image_"+mapCurLoc).show();
+	});
+
+	//put us on the right floor to begin with
+	mapCurLoc = baseGetLocation();
+	if (mapCurLoc == "0") mapCurLoc = "3N";
+	$("#map_floor_chooser_button_"+mapCurLoc.charAt(0)).addClass("on").click();
+
+	//any clicks not on the map popup close the map popup
+	$("body").mousedown(function(e) {
+		if ($(e.target).is("#map_popup")) return;
+		if ($(e.target).parents("#map_popup").length!=0) return;
+		$("#map_popup").hide();
+	});
+
+});
